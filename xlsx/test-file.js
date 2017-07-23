710114cd0699c4824b80786f06ece10edbee863b
NoName4Me <anich.evas@gmail.com>
2017-07-24 00:30:00 +0800

    add output files.

---
+++ xlsx/test-file.js
@@ -0,0 +1,10 @@
+/**
+ * test function
+ */
+function hi() {
+    var say_hello_enUS = "this is test"; // en-us
+    var hello1 = "jonge";
+    var dog;
+    var say_hello_enZH = "this is called 'test'"; // en-us
+    var hello2 = "hi";
+}
