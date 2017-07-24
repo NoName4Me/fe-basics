2a0b7ce2235dea021b02e92ae2a3d27f571ab50a
NoName4Me <anich.evas@gmail.com>
2017-07-25 00:59:12 +0800

    add logic

--- xlsx/main.js
+++ xlsx/main.js
@@ -1,3 +1,4 @@
+var srcData, templateData;
 document.querySelector("#srcFile").onchange = function(event) {
     console.log(event);
     var file = event.target.files[0];
@@ -5,26 +6,32 @@ document.querySelector("#srcFile").onchange = function(event) {
     reader.onload = function(re) {
         // The file's text will be printed here
         console.log(re)
-        try {
-            var data = re.target.result,
-                workbook = XLSX.read(data, {
-                    type: 'binary'
-                });
-                var fromTo = '';
-
-                for (var sheet in workbook.Sheets) {
-                    if (workbook.Sheets.hasOwnProperty(sheet)) {
-                        fromTo = workbook.Sheets[sheet]['!ref'];
-                        console.log(fromTo);
-                        var allData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]);
-                        break; // 如果只取第一张表，就取消注释这行
-                    }
-                }
-        } catch (error) {
-            console.error(error);
-            return;
-        }
+        srcData = re.target.result;
 
     };
     reader.readAsBinaryString(file);
+}
+
+function ok() {
+    if(!srcData || !templateData) {
+        alert("check your "+ (srcData ? "template" : (templateData ? "data":"data and template") + " file!"));
+    }
+    try {
+        var workbook = XLSX.read(srcData, {
+            type: 'binary'
+        });
+        var fromTo = '';
+
+        for (var sheet in workbook.Sheets) {
+            if (workbook.Sheets.hasOwnProperty(sheet)) {
+                fromTo = workbook.Sheets[sheet]['!ref'];
+                console.log(fromTo);
+                var allData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]);
+                break; // 如果只取第一张表，就取消注释这行
+            }
+        }
+    } catch (error) {
+        console.error(error);
+        return;
+    }
 }
