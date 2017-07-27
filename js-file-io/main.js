9aea13191aa00065e20616b5e3139246f306d88e
NoName4Me <anich.evas@gmail.com>
2017-07-28 07:50:53 +0800

    post file.

--- js-file-io/main.js
+++ js-file-io/main.js
@@ -1,20 +1,27 @@
 var srcData, templateData;
 document.querySelector("#srcFile").onchange = function(event) {
-    console.log(event);
+    //console.log(event);
     var file = event.target.files[0];
     var reader = new FileReader();
     reader.onload = function(re) {
         // The file's text will be printed here
-        console.log(re)
+        //console.log(re)
         srcData = re.target.result;
-
+        $http.post("./send", {type:'src', data: srcData})
+            .then(JSON.parse)
+            .then((r) => {
+                console.log(r);
+            })
+            .catch(function(error) {
+                console.error(error);
+            });
     };
     reader.readAsBinaryString(file);
 }
 
 function ok() {
-    if(!srcData || !templateData) {
-        alert("check your "+ (srcData ? "template" : (templateData ? "data":"data and template") + " file!"));
+    if (!srcData || !templateData) {
+        alert("check your " + (srcData ? "template" : (templateData ? "data" : "data and template") + " file!"));
     }
     try {
         var workbook = XLSX.read(srcData, {
@@ -35,3 +42,31 @@ function ok() {
         return;
     }
 }
+
+var $http = {
+    basic: function(url, method, headers, data) {
+        return new Promise(function(resolve, reject) {
+            let req = new XMLHttpRequest();
+            req.open(method, url);
+            req.onload = function() {
+                if (req.status === 200) {
+                    resolve(req.response);
+                } else {
+                    reject(new Error(req.statusText));
+                }
+            };
+
+            req.onerror = function() {
+                reject(new Error("Network error"));
+            };
+
+            req.send();
+        });
+    },
+    get: function(url) {
+        return $http.basic(url, 'GET');
+    },
+    post: function(url, data) {
+        return $http.basic(url, 'POST', data);
+    }
+};
--- js-file-io/server.js
+++ js-file-io/server.js
@@ -1,34 +1,71 @@
 const http = require("http");
 const fs = require('fs');
 const url = require('url');
+const XLSX = require('xlsx');
 
 exports.start = function() {
     http.createServer(function(request, response) {
-        var pathname = url.parse(request.url).pathname;
-        var ext = pathname.match(/(\.[^.]+|)$/)[0];
-        if (['.css', '.html', '.js', '.png'].indexOf(ext) < 0) {
-            response.writeHead(200, {
-                "Content-Type": "text/plain"
-            });
-            response.write("not support '" + ext + "' format!");
-            response.end();
-        } else {
-            fs.readFile("." + request.url, ext === '.png' ? null : 'utf-8', function(err, data) {
-                if (err) {
-                    throw err;
-                };
+        if (request.method === 'GET') {
+            var pathname = url.parse(request.url).pathname;
+            var ext = pathname.match(/(\.[^.]+|)$/)[0];
+            if (['.css', '.html', '.js', '.png'].indexOf(ext) < 0) {
                 response.writeHead(200, {
-                    "Content-Type": {
-                        ".css": "text/css",
-                        ".js": "application/javascript",
-                        ".html": "text/html",
-                        ".png": "image/png"
-                    }[ext]
+                    "Content-Type": "text/plain"
                 });
-                response.write(data);
+                response.write(JSON.stringify({
+                    errorCode: 1001,
+                    msg: "not support '" + ext + "' format!"
+                }));
                 response.end();
+            } else {
+                fs.readFile("." + request.url, ext === '.png' ? null : 'utf-8', function(err, data) {
+                    if (err) {
+                        throw err;
+                    };
+                    response.writeHead(200, {
+                        "Content-Type": {
+                            ".css": "text/css",
+                            ".js": "application/javascript",
+                            ".html": "text/html",
+                            ".png": "image/png"
+                        }[ext]
+                    });
+                    response.write(data);
+                    response.end();
+                });
+            }
+        } else {
+            console.log('get a post: ', request.url);
+            let body = [];
+            request.on('error', (err) => {
+                console.error(err);
+            }).on('data', (chunk) => {
+                body.push(chunk);
+            }).on('end', () => {
+                body = Buffer.concat(body).toString();
+                // At this point, we have the headers, method, url and body, and can now
+                // do whatever we need to in order to respond to this request.
+                try {
+                    var workbook = XLSX.read(JSON.parse(body).data, {
+                        type: 'binary'
+                    });
+                    var fromTo = '';
+
+                    for (var sheet in workbook.Sheets) {
+                        if (workbook.Sheets.hasOwnProperty(sheet)) {
+                            fromTo = workbook.Sheets[sheet]['!ref'];
+                            console.log(fromTo);
+                            var allData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]);
+                            break; // 如果只取第一张表，就取消注释这行
+                        }
+                    }
+                } catch (error) {
+                    console.error(error);
+                    return;
+                }
             });
         }
+
 
     }).listen(8888);
 };
